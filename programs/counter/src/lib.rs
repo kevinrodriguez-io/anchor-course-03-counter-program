675664f5be532e612bc12ea28e5297921e523f9b
Mister Kevin <misterkevin@iMac-Pro.local>
2022-02-21 02:11:22 -0600

    feat: with pdas

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -2,42 +2,72 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+const COUNTER_SEED: &[u8] = b"counter";
+
 #[program]
 pub mod counter {
     use super::*;
+    
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
-        counter.update_authority = *ctx.accounts.update_authority.key;
         Ok(())
     }
-    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+
+    pub fn increment(ctx: Context<Increment>, _counter_bump: u8) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
         counter.count = counter.count + 1;
+        Ok(())
+    }
+
+    pub fn close(_ctx: Context<CloseCounter>, _counter_bump: u8) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = update_authority)]
+    #[account(
+        init,
+        payer = payer,
+        seeds = [COUNTER_SEED, payer.key().as_ref()],
+        bump
+    )]
     pub counter: Account<'info, Counter>,
     #[account(mut)]
-    pub update_authority: Signer<'info>, // Payer
-    pub system_program: Program<'info, System>, // Create Account IX (Counter)
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
+#[instruction(counter_bump: u8)]
 pub struct Increment<'info> {
-    #[account(mut, has_one = update_authority)]
+    #[account(
+        mut,
+        seeds = [COUNTER_SEED, payer.key().as_ref()],
+        bump = counter_bump
+    )]
     pub counter: Account<'info, Counter>,
     #[account()]
-    pub update_authority: Signer<'info>, // Payer
+    pub payer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(counter_bump: u8)]
+pub struct CloseCounter<'info> {
+    #[account(
+        mut,
+        close = payer,
+        seeds = [COUNTER_SEED, payer.key().as_ref()],
+        bump = counter_bump,
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
 }
 
 #[account]
 #[derive(Default)]
 pub struct Counter {
-    count: u64,               // 8
-    update_authority: Pubkey, // 32
+    count: u64, // 8
 }
--- tests/counter.ts
+++ tests/counter.ts
@@ -1,7 +1,11 @@
 import * as anchor from "@project-serum/anchor";
-import { ACCOUNT_DISCRIMINATOR_SIZE, Program } from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
 import { expect } from "chai";
+import { TextEncoder } from "util";
 import { Counter } from "../target/types/counter";
+
+const b = (input: TemplateStringsArray) =>
+  new TextEncoder().encode(input.join(""));
 
 describe("counter", () => {
   // Configure the client to use the local cluster.
@@ -10,104 +14,66 @@ describe("counter", () => {
   const program = anchor.workspace.Counter as Program<Counter>;
   const localWallet = anchor.Wallet.local();
 
-  let counterKeypair: anchor.web3.Keypair | null = null;
-  it("initializes the counter", async () => {
-    counterKeypair = anchor.web3.Keypair.generate();
-    // Add your test here.
-    const tx = await program.rpc.initialize({
-      accounts: {
-        counter: counterKeypair.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        updateAuthority: localWallet.publicKey,
-      },
-      signers: [localWallet.payer, counterKeypair],
-    });
-    console.log("Your transaction signature", tx);
-    const counter = await program.account.counter.fetch(
-      counterKeypair.publicKey
-    );
-    expect(new anchor.BN(0).eq(counter.count)).to.be.true;
-    expect(counter.updateAuthority.equals(localWallet.publicKey)).to.be.true;
-  });
+  let counterKey: anchor.web3.PublicKey | null = null;
+  let counterBump: number | null = null;
 
-  it("increases the counter", async () => {
-    expect(counterKeypair).not.to.be.null;
-    const tx = await program.rpc.increment({
+  it("initializes the counter", async () => {
+    const [derivedAddress, bump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [b`counter`, localWallet.publicKey.toBytes()],
+        program.programId
+      );
+    counterKey = derivedAddress;
+    counterBump = bump;
+
+    const _tx = await program.rpc.initialize({
       accounts: {
-        counter: counterKeypair.publicKey,
-        updateAuthority: localWallet.publicKey,
+        counter: counterKey,
+        payer: localWallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
       },
       signers: [localWallet.payer],
     });
-    console.log("Your transaction signature", tx);
-    const counter = await program.account.counter.fetch(
-      counterKeypair.publicKey
-    );
+    const counter = await program.account.counter.fetch(counterKey);
+    expect(new anchor.BN(0).eq(counter.count)).to.be.true;
+  });
+
+  it("increases the counter", async () => {
+    expect(counterKey).not.to.be.null;
+    const _tx = await program.rpc.increment(counterBump, {
+      accounts: {
+        counter: counterKey,
+        payer: localWallet.publicKey,
+      },
+      signers: [localWallet.payer],
+    });
+    const counter = await program.account.counter.fetch(counterKey);
     expect(new anchor.BN(1).eq(counter.count)).to.be.true;
-    expect(counter.updateAuthority.equals(localWallet.publicKey)).to.be.true;
   });
 
   it("fails if we try to increase the counter as someone else", async () => {
     const otherWallet = anchor.web3.Keypair.generate();
     expect(
-      program.rpc.increment({
+      program.rpc.increment(counterBump, {
         accounts: {
-          counter: counterKeypair.publicKey,
-          updateAuthority: otherWallet.publicKey,
+          counter: counterKey,
+          payer: localWallet.publicKey,
         },
         signers: [otherWallet],
       })
     ).to.throw;
   });
 
-  // We spotted a design flow:
-  // There can be more than two counter instances for that user!!!!!!!!!
-  it("counter accounts should be 1", async () => {
-    const counters = await program.account.counter.all([
-      {
-        memcmp: {
-          offset: ACCOUNT_DISCRIMINATOR_SIZE + 8, // u64 counter
-          bytes: localWallet.publicKey.toBase58(),
-        },
-      },
-    ]);
-    console.log(counters);
-    expect(counters?.[0]?.account?.count).not.to.be.undefined;
-    expect(counters.length).to.be.eq(1);
-  });
-
-  it("initializes another counter", async () => {
-    const anotherCounterKeypair = anchor.web3.Keypair.generate();
-    // Add your test here.
-    const tx = await program.rpc.initialize({
+  it("closes the counter", async () => {
+    expect(counterKey).not.to.be.null;
+    const _tx = await program.rpc.close(counterBump, {
       accounts: {
-        counter: anotherCounterKeypair.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        updateAuthority: localWallet.publicKey,
+        counter: counterKey,
+        payer: localWallet.publicKey,
       },
-      signers: [localWallet.payer, anotherCounterKeypair],
+      signers: [localWallet.payer],
     });
-    console.log("Your transaction signature", tx);
-    const counter = await program.account.counter.fetch(
-      anotherCounterKeypair.publicKey
-    );
-    expect(new anchor.BN(0).eq(counter.count)).to.be.true;
-    expect(counter.updateAuthority.equals(localWallet.publicKey)).to.be.true;
-  });
-
-  // We spotted a design flow:
-  // There can be more than two counter instances for that user!!!!!!!!!
-  it("counter accounts should still be 1", async () => {
-    const counters = await program.account.counter.all([
-      {
-        memcmp: {
-          offset: ACCOUNT_DISCRIMINATOR_SIZE + 8, // u64 counter
-          bytes: localWallet.publicKey.toBase58(),
-        },
-      },
-    ]);
-    console.log(counters);
-    expect(counters?.[0]?.account?.count).not.to.be.undefined;
-    expect(counters.length).to.be.eq(1);
+    const counter = await program.account.counter.fetchNullable(counterKey);
+    expect(counter).to.be.null;
   });
 });
